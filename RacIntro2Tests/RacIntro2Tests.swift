51c2a6eaef8fca9d2a1881ab5efcf55cca085b42
Nahuel Marisi <nahuelmarisi@gmail.com>
2017-03-25 17:44:16 +0000

    Initial Commit

---
+++ RacIntro2Tests/RacIntro2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  RacIntro2Tests.swift
+//  RacIntro2Tests
+//
+//  Created by Nahuel Marisi on 2017-03-25.
+//  Copyright © 2017 Techbrewers Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import RacIntro2
+
+class RacIntro2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
